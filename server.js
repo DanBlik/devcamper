2df88c75c0b5f76554bee25815425768f48750f3
danblik <DDBlikov@sberbank.ru>
2023-06-13 16:48:24 +0300

    create logger middleware and use morgan fot logging

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 
 import routes from './routes/bootcamps.js'
 
@@ -8,6 +9,10 @@ dotenv.config({ path: './configs/config.env' })
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use('/api/v1/bootcamps', routes)
 
